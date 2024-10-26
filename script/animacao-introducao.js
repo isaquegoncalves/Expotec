dbaf41d5e50cd152b41db09094c1de560d069ba1
isaquegoncalves <114412444+isaquegoncalves@users.noreply.github.com>
2024-10-25 21:55:12 -0300

    Versão 2.2
    Adição de animação de introdução.

---
+++ script/animacao-introducao.js
@@ -0,0 +1,23 @@
+//============FUNDO-INICIO===========>
+    setTimeout(function(){
+        const foto = document.getElementById('fundo-inicio');
+        foto.classList.add('ativo');
+    }, 500);
+
+//============HEADER > H2===========>
+    setTimeout(function(){
+        const foto = document.getElementById('header-h2');
+        foto.classList.add('ativo');
+    }, 800);
+
+//============HEADER > H3===========>
+    setTimeout(function(){
+        const foto = document.getElementById('header-h3');
+        foto.classList.add('ativo');
+    }, 1900);
+
+//============HEADER > DESC===========>
+    setTimeout(function(){
+        const foto = document.getElementById('header-desc');
+        foto.classList.add('ativo');
+    }, 1100);
